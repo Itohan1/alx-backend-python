14efc951d83975b21f960a020be8e7d96021f0b4
Itohan <momoduitohan5@gmail.com>
2024-08-01 20:46:54 +0100

    Add new file on Mapping

--- 0x00-python_variable_annotations/101-safely_get_value.py
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -12,6 +12,7 @@ def safely_get_value(
         dct: Mapping,
         key: Any, default: Union[T, None]
         ) -> Union[Any, T]:
+    """return values, add type annotations to the function"""
     if key in dct:
         return dct[key]
     else:
