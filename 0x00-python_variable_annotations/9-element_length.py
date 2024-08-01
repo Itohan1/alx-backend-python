a830d19394f46731cb065d5b92c8c55142ce92b6
Itohan <momoduitohan5@gmail.com>
2024-08-01 18:23:55 +0100

    Add new file on Iterables

--- 0x00-python_variable_annotations/9-element_length.py
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -4,8 +4,9 @@
    parameters and return values
    with the appropriate types
 """
-import typing
+from typing import Iterable, Sequence, List, Tuple
 
 
-def element_length(lst: typing.Iterable[typing.Sequence]) -> typing.List[typing.Tuple[typing.Sequence, int]]:
+def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
+    """return values with the appropriate types"""
     return [(i, len(i)) for i in lst]
