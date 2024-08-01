32e5f54d9aea26a617eb988bb18f86f2792c6791
Itohan <momoduitohan5@gmail.com>
2024-08-01 21:38:39 +0100

    Using mypy to run a file

--- 0x00-python_variable_annotations/102-type_checking.py
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -6,14 +6,14 @@
 from typing import Tuple, Any, List
 
 
-def zoom_array(lst: Tuple, factor: int = 2) -> Tuple[Any, ...]:
+def zoom_array(lst: Tuple, factor: int = 2) -> List:
     """Applying changes"""
 
     zoomed_in: List[Any] = [
         item for item in lst
         for _ in range(factor)
     ]
-    return tuple(zoomed_in)
+    return list(zoomed_in)
 
 
 array = (12, 72, 91)
